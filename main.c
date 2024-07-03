795bc059db9b95843b4e9e64b24c1f89c314b607
CoutHi <alicftc@proton.me>
2024-07-03 19:14:29 +0200

    Now prints the image and display the information to the right of it

--- main.c
+++ main.c
@@ -3,16 +3,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-void find_string(char* dest, char* source,const char* string, int buffer_size){
-    char* start_pos = strstr(source, string);
-    if(start_pos != NULL){
-        start_pos += strlen(string) + 1; // Move to start of the actual string
-        char* end_pos = strchr(start_pos, '"');
-        if(end_pos != NULL){
-            *end_pos = '\0'; // Null-terminate the string
-            strncpy(dest, start_pos, buffer_size); // Copy up to 127 characters
-        }
+void print_spaces(){
+    char d = ' ';
+    int i = 0;
+    for(i = 0; i < 55; i++){
+        printf("%c",d);
     }
+}
+
+void find_string(char* dest, char* source,const char* string, int buffer_size){
+
+    char* start_pos = strstr(source, string);
+    int i = 0;
+    int c = 0;
+
+    if(start_pos){
+        start_pos += strlen(string) + 1;
+        i = start_pos - source;
+
+        while(source[i] != '"' && i < buffer_size-1){
+        dest[c] = source[i];
+        i++;
+        c++;
+        }
+    }else{
+        printf("String %s not found!", string);
+        exit(1);
+    }
+
+    dest[c+1] = '\0';
 }
 void malloc_check(char* x, const char* msg){
     if(x == NULL){
@@ -38,13 +57,13 @@ int main(){
     }
 
     char* result;
-    result = malloc(sizeof(char)*512);
+    result = malloc(sizeof(char)*1024);
    
     malloc_check(result,"Result");
 
     int c;
     int i = 0;
-    while((c = fgetc(fp)) != EOF && i < 511){
+    while((c = fgetc(fp)) != EOF && i < 1023){
         result[i] = (char)c;
         i++;
     }
@@ -60,15 +79,15 @@ int main(){
     malloc_check(distro, "Distro");
 
     find_string(distro, result, "PRETTY_NAME=", 128);
+    distro[strlen(distro + 1)] = '\0';
 
-    printf("DISTRIBUTION: %s\n",distro);
-    free(distro);
+    print_spaces();
+    printf("Distribution: %s\n",distro);
 
     char* kernel;
     kernel = malloc(sizeof(char)*128);
     malloc_check(kernel, "Kernel");
 
-    
     command[0] = '\0';
     snprintf(command, 512, "uname -r");
 
@@ -82,19 +101,60 @@ int main(){
 
     c = 0;
     i = 0;
-    while((c = fgetc(fp)) != EOF && i < 511){
+    while((c = fgetc(fp)) != EOF && i < 511 && c != '-'){
         kernel[i] = (char)c;
         i++;
     }
     kernel[i] = '\0';
-    free(command);
 
     if(pclose(fp) == -1){
         printf("pclose failed!");
         return 1;
     }
 
-    printf("KERNEL: %s",kernel);
+    print_spaces();
+    printf("Kernel Version: %s\n",kernel);
+    free(kernel);
+
+    char* distro_url;
+    distro_url = malloc(sizeof(char)*256);
+    malloc_check(distro_url, "Distro_URL");
+
+    find_string(distro_url, result, "HOME_URL=", 256);
+    print_spaces();
+    printf("URL: %s\n",distro_url);
+    free(distro_url);
+
+    char* documentation;
+    documentation = malloc(sizeof(char)*256);
+
+    find_string(documentation, result, "DOCUMENTATION_URL=", 256);
+    print_spaces();
+    printf("Documentation: %s\n", documentation);
+    free(documentation);
+
+    command[0] = '\0';
+
+    char* image;
+    image = malloc(sizeof(char)*64);
+    malloc_check(image, "Image");
+
+    i = 0;
+    c = 0;
+    while((distro[i] != '-' && distro[i] != ' ') && i < 63){
+        image[c] = distro[i];
+        i++;
+        c++;
+    }
+    image[c] = '\0';
+
+    snprintf(command, 512, "chafa --view-size 52x52 ./images/%s-linux.png", image);
+    system(command);
+    free(command);
+    free(distro);
+    free(image);
+
+
 }
 
 
