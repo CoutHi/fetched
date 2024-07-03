14591d57131c1e82e157d0da7a679e00374c80a5
CoutHi <alicftc@proton.me>
2024-07-03 02:16:48 +0200

    improved logic, added the kernel version and distribution name to the output

--- main.c
+++ main.c
@@ -3,6 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+void find_string(char* dest, char* source,const char* string, int buffer_size){
+    char* start_pos = strstr(source, string);
+    if(start_pos != NULL){
+        start_pos += strlen(string) + 1; // Move to start of the actual string
+        char* end_pos = strchr(start_pos, '"');
+        if(end_pos != NULL){
+            *end_pos = '\0'; // Null-terminate the string
+            strncpy(dest, start_pos, buffer_size); // Copy up to 127 characters
+        }
+    }
+}
 void malloc_check(char* x, const char* msg){
     if(x == NULL){
         printf("Memory Allocation failed for %s",msg);
@@ -15,7 +26,6 @@ int main(){
 
     char* command;
     command = malloc(sizeof(char)*512);
-
     malloc_check(command, "Command");
    
     snprintf(command, 512, "cat /etc/*-release");
@@ -39,7 +49,6 @@ int main(){
         i++;
     }
     result[i] = '\0';
-    free(command);
 
     if(pclose(fp) == -1){
         printf("pclose failed");
@@ -48,10 +57,44 @@ int main(){
 
     char* distro;
     distro = malloc(sizeof(char)*128);
-    
     malloc_check(distro, "Distro");
 
-    printf("%s",result);
+    find_string(distro, result, "PRETTY_NAME=", 128);
+
+    printf("DISTRIBUTION: %s\n",distro);
+    free(distro);
+
+    char* kernel;
+    kernel = malloc(sizeof(char)*128);
+    malloc_check(kernel, "Kernel");
+
+    
+    command[0] = '\0';
+    snprintf(command, 512, "uname -r");
+
+    fp = popen(command, "r");
+
+    if(fp == NULL){
+        perror("Command failed");
+        free(command);
+        return 1;
+    }
+
+    c = 0;
+    i = 0;
+    while((c = fgetc(fp)) != EOF && i < 511){
+        kernel[i] = (char)c;
+        i++;
+    }
+    kernel[i] = '\0';
+    free(command);
+
+    if(pclose(fp) == -1){
+        printf("pclose failed!");
+        return 1;
+    }
+
+    printf("KERNEL: %s",kernel);
 }
 
 
